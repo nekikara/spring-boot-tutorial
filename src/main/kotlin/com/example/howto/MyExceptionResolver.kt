c73fdf15cc9d14bb0e5dfe6054c798f4ef125b0a
Ken Araki <nekikara@gmail.com>
2017-08-10 13:21:42 +0200

    Return a json error if a request is an api
    $ curl http://localhost:8080/api
    {"timestamp":1430490748092,"status":400,"error":"Bad Request","exception":"sample.springboot.web.MyException","message":"test exception","path":"/api"}
    
    $ curl http://localhost:8080/api/null
    {"timestamp":1430490749586,"status":500,"error":"Internal Server Error","exception":"java.lang.NullPointerException","message":"test exception","path":"/api/null"}

--- src/main/kotlin/com/example/howto/MyExceptionResolver.kt
+++ src/main/kotlin/com/example/howto/MyExceptionResolver.kt
@@ -6,16 +6,31 @@ import org.springframework.web.servlet.ModelAndView
 import java.lang.Exception
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import org.springframework.web.bind.annotation.RestController
+import com.fasterxml.jackson.databind.util.ClassUtil.getDeclaringClass
+import org.springframework.web.method.HandlerMethod
+
 
 @Component
 class MyExceptionResolver : HandlerExceptionResolver {
 
-    override fun resolveException(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception): ModelAndView {
-        println(ex.javaClass.toString() + " : " + ex.message)
+    override fun resolveException(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception): ModelAndView? {
+        if (isRestController(handler)) {
+            return null
+        }
 
         val mv = ModelAndView()
         mv.viewName = "my-error"
 
         return mv
+    }
+
+    private fun isRestController(handler: Any): Boolean {
+        if (handler is HandlerMethod) {
+
+            return handler.method.declaringClass.isAnnotationPresent(RestController::class.java)
+        }
+
+        return false
     }
 }
