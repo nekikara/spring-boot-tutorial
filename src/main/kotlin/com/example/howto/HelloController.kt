3cb59eb0323a76456394a15d5d38c2d6afe5abcc
Ken Araki <nekikara@gmail.com>
2017-08-10 13:42:31 +0200

    Use template engine
    Please, access the following link
    http://localhost:8080/hello

--- src/main/kotlin/com/example/howto/HelloController.kt
+++ src/main/kotlin/com/example/howto/HelloController.kt
@@ -1,14 +1,13 @@
 package com.example.howto
 
 import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 class HelloController {
 
     @RequestMapping(method = arrayOf(RequestMethod.GET))
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun getMethod() {
-    }
+    fun hello() = "hello"
 }
