be9a1610eb9b7e0d405df5b5c6fe4a71c8b79003
Ken Araki <nekikara@gmail.com>
2017-08-10 13:24:38 +0200

    Add error handler for an request action
    $ curl http://localhost:8080/api/null
    {"message":"test exception"}":500,"error":"Internal Server Error","exception":"java.lang.NullPointerException","message":"test exception","path":"/api/null"}

--- src/main/kotlin/com/example/howto/WebApiController.kt
+++ src/main/kotlin/com/example/howto/WebApiController.kt
@@ -1,5 +1,6 @@
 package com.example.howto
 
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -16,6 +17,11 @@ class WebApiController {
     @RequestMapping(value="/null", method = arrayOf(RequestMethod.GET))
     fun method2() {
         throw NullPointerException("test exception")
+    }
+
+    @ExceptionHandler(NullPointerException::class)
+    fun handling(e: NullPointerException): String {
+        return "{\"message\":\"" + e.message + "\"}"
     }
 
 }
