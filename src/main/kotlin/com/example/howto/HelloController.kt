48f4984d59ba063af925241a7b010b39aba146d5
Ken Araki <nekikara@gmail.com>
2017-08-10 14:19:06 +0200

    Use iteration parts
    Access http://localhost:8080/hello

--- src/main/kotlin/com/example/howto/HelloController.kt
+++ src/main/kotlin/com/example/howto/HelloController.kt
@@ -3,6 +3,7 @@ package com.example.howto
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @Controller
 @RequestMapping("/hello")
@@ -10,12 +11,13 @@ class HelloController {
 
     @RequestMapping(method = arrayOf(RequestMethod.GET))
     fun hello(model: Model): String {
-        var hoge = Hoge()
+        val list = listOf(
+            mapOf("id" to 10, "value" to "hoge"),
+            mapOf("id" to 20, "value" to "tore"),
+            mapOf("id" to 30, "value" to "get")
+        ).map { Hoge(it.get("id") as Int?, it.get("value") as String?) }
 
-        hoge.id = 10
-        hoge.value = "hoge"
-
-        model.addAttribute("myData", hoge)
+        model.addAttribute("hogeList", list)
 
         return "hello"
     }
